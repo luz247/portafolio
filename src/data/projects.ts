e8fbebb8695173ec26b97eeb3f6596f2378bd87a
luz <marivellinanvalerioo@gmail.com>
2026-02-09 16:50:03 -0300

    sencond-commit

---
+++ src/data/projects.ts
@@ -0,0 +1,127 @@
+export const projects = [
+  {
+    slug: "crm-call-center-v0",
+    title: "CRM de Call Center v0.0",
+    shortDesc: "Gestión de llamadas, agenda, reportes y automatización.",
+    description:
+      "CRM para call center con agenda, gestión de contactos, reportes y automatización de procesos. Incluye integración con APIs y base de datos.",
+    tags: ["React", "TypeScript", "Tailwindcss", "Django", "SQL Server"],
+    cover: "/images/projects/crm/crm-1.png",
+    gallery: [
+      "/images/projects/crm/crm-2.png",
+      "/images/projects/crm/agenda-1.png",
+      "/images/projects/crm/agenda-3.png",
+      "/images/projects/crm/agenda-2.png",
+    ],
+    links: {
+      demo: "https://crm-prueba-new.netlify.app/login",
+      github: "https://github.com/luz247/crm-v0-project.git",
+    },
+    categories: ["fullstack", "frontend"],
+  },
+
+  {
+    slug: "crm-call-center-v1",
+    title: "CRM de Call Center v0.1",
+    shortDesc: "Gestión de llamadas, agenda, reportes y automatización.",
+    description:
+      "CRM para call center con agenda, gestión de contactos, reportes y automatización de procesos. Incluye integración con APIs y base de datos.",
+    tags: ["Vite React","Redux", "TypeScript", "Tailwindcss", "Django", "SQL Server"],
+    cover: "/images/projects/crm/crm-v0-1.png",
+    gallery: [
+      "/images/projects/crm/crm-v0-1.png",
+      "/images/projects/crm/crm-v0-2.png",
+    ],
+    links: {
+      demo: "https://crm-outbound-wecollect.netlify.app/acsa?MODO=INBOUND&discador=vici&vendor_id=3555201-4&phone_number=927249744&user=26704505-4&id_producto=&lead_id=580748&uniqueid=1717439373.489849&list_id=101&campaign=PRUEBA",
+      github: "https://github.com/luz247/crm-project-v0-01.git",
+    },
+    categories: ["fullstack", "frontend"],
+  },
+
+  {
+    slug: "login-cobranza-v0",
+    title: "Login Conbranza",
+    shortDesc: "Flujos con Webhooks, integraciones y respuestas automáticas.",
+    description: "Automatización con WhatsApp API y NestJS. Gestión de sesiones inteligentes mediante Redis para evitar reinicios de flujo y asegurar la continuidad de la conversación.",
+    tags: ["vite", "React","Router"],
+    cover: "/images/projects/login/v0/login-v0.png",
+    gallery: ["/images/projects/login/v0/login-v0.png"],
+    links: { demo: "https://vocal-tiramisu-4231e4.netlify.app", github: "https://github.com/luz247/front-react-vite-login-conbranza.git" },
+    categories: ["frontend"]
+  },
+
+  {
+    slug: "automatizacion-whatsapp",
+    title: "Automatización WhatsApp",
+    shortDesc: "Flujos con Webhooks, integraciones y respuestas automáticas.",
+    description: "Automatización con WhatsApp API y NestJS. Gestión de sesiones inteligentes mediante Redis para evitar reinicios de flujo y asegurar la continuidad de la conversación.",
+    tags: ["NestJS", "Redis", "WhatsApp Cloud API"],
+    cover: "/images/projects/crm/agen.png",
+    gallery: ["/images/projects/meta-whatsapp/",""],
+    links: { demo: "https://vocal-tiramisu-4231e4.netlify.app/", github: "https://github.com/luz247/front-react-vite-login-conbranza.git" },
+    categories: ["automatization"]
+  },
+  {
+    slug: "pipelines-sftp-nas",
+    title: "Pipelines SFTP / NAS",
+    shortDesc: "Validación y transferencia de archivos con reportes.",
+    description:
+      "Pipeline para validar existencia de archivos por fecha, transferirlos por SFTP/NAS y generar reportes automáticos por correo.",
+    tags: ["Python", "SFTP", "ETL"],
+    cover:"/images/projects/crm/agenda-2.png",
+    gallery: ["/images/projects/crm/agenda-2.png",],
+    links: { demo: "", github: "" },
+    categories: [ "automation"],
+  },
+  {
+    slug: "pokedex",
+    title: "Pokedex (Astro)",
+    shortDesc: "App en Astro consumiendo API y mostrando Pokemons.",
+    description:
+      "Proyecto en Astro con TypeScript, consumo de API y páginas optimizadas.",
+    tags: ["Astro", "TypeScript"],
+    cover: "/images/projects/pokedex/poke-1.png",
+    gallery: [
+      "/images/projects/pokedex/poke-2.png",
+      "/images/projects/pokedex/poke-3.png",
+      "/images/projects/pokedex/poke-4.png",
+    ],
+    links: { demo: "https://amazing-lily-8bd277.netlify.app/", github: "" },
+    categories: ["frontend", "portfolio",'backend'],
+  },
+
+    {
+    slug: "pokedex",
+    title: "Pokedex (Astro)",
+    shortDesc: "App en Astro consumiendo API y mostrando Pokemons.",
+    description:
+      "Proyecto en Astro con TypeScript, consumo de API y páginas optimizadas.",
+    tags: ["Astro", "TypeScript"],
+    cover: "/images/projects/pokedex/poke-1.png",
+    gallery: [
+      "/images/projects/pokedex/poke-2.png",
+      "/images/projects/pokedex/poke-3.png",
+      "/images/projects/pokedex/poke-4.png",
+    ],
+    links: { demo: "https://amazing-lily-8bd277.netlify.app/", github: "" },
+    categories: ["frontend", "portfolio",'backend'],
+  },
+
+    {
+    slug: "pokedex",
+    title: "Pokedex (Astro)",
+    shortDesc: "App en Astro consumiendo API y mostrando Pokemons.",
+    description:
+      "Proyecto en Astro con TypeScript, consumo de API y páginas optimizadas.",
+    tags: ["Astro", "TypeScript"],
+    cover: "/images/projects/pokedex/poke-1.png",
+    gallery: [
+      "/images/projects/pokedex/poke-2.png",
+      "/images/projects/pokedex/poke-3.png",
+      "/images/projects/pokedex/poke-4.png",
+    ],
+    links: { demo: "https://amazing-lily-8bd277.netlify.app/", github: "" },
+    categories: ["frontend", "portfolio",'backend'],
+  },
+];
